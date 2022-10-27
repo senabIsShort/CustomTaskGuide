d32ebbb4d52f65215aa059e395c2788f26c981ec
Abdurrahman SEN <sen.abdurrahman1@gmail.com>
2022-10-27 16:45:07 +0200

    Footer Typo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -162,7 +162,7 @@ function App () {
         Made with <span aria-label="love">💖</span> using <a href="https://create-react-app.dev/"> create-react-app</a> and <a href="https://reactjs.org/"> React</a>
       </div>
       <div>
-        You can check out my other project at <a href="https://senabisshort.github.io">senabisshort.github.io</a>
+        You can check out my projects at <a href="https://senabisshort.github.io">senabisshort.github.io</a>
       </div>
     </footer>
     <Modal 
