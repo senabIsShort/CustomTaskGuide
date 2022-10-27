32db938f8ec654aafc8e1e3fac4218ee82cfd004
Abdurrahman SEN <sen.abdurrahman1@gmail.com>
2022-10-27 15:17:10 +0200

    Added footer element + fix layout accordingly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -157,6 +157,14 @@ function App () {
     <main>
       {renderItems()}
     </main>
+    <footer>
+      <div>
+        Made with <span aria-label="love">💖</span> using <a href="https://create-react-app.dev/"> create-react-app</a> and <a href="https://reactjs.org/"> React</a>
+      </div>
+      <div>
+        You can check out my other project at <a href="https://senabisshort.github.io">senabisshort.github.io</a>
+      </div>
+    </footer>
     <Modal 
       visibility={visibility}
       close={toggle}
